b6ab98736ad76fc0971406180127f3025c12a528
unknown <carlos.alvarenga017@gmail.com>
2023-02-16 12:50:57 -0600

    advanced during class

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Cuervana</Text> 
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header : {
+    height : '50%',
+    width : '100%',
+    display : 'flex',
+    alignItems : 'center',
+    justifyContent : 'center',
+    backgroundColor : '#063970'
   },
+  headerText : {
+    fontSize : '30px',
+    color : 'white'
+  }
 });
